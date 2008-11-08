5da8c52391deaf5133696ff25cc4460a63f60c32
Malte S. Stretz <mss@apache.org>
2008-11-09 00:37:25 +0100

    Translated to English.

--- src/minidmx.c
+++ src/minidmx.c
@@ -1,27 +1,32 @@
-//===========================================================================
-// Dateiname : minidmx.c
-// Funktion  : DMX-Ausgang für den PC
-// Compiler  : WinAVR (http://sourceforge.net/projects/winavr/)
-// MCU       : AT90S2313 ATTiny2313
-// Quarz     : 9,216 MHz
-// Änderungen: 18.02.2001 - V1.0
-//             - Erste öffentliche Version
-//             05.02.2003 - V1.1
-//             - LED1 zeigt nun ein erfolgreiches Senden der DMX Daten an
-//             - LED2 zeigt Fehler beim Datenempfang vom PC an
-//             - LED3 zeigt das Ende das Startvorgangs an (Daten können nun
-//               gesendet werden)
-//             21.03.2003 - V2.0 Testversion
-//             - Der Quelltext der Firmware wurde in der Programmiersprache
-//               C neu geschrieben
-//             09.09.2003 - V2.0
-//             - Fehler in der Generierung des DMX-Signals behoben
-//             17.12.2006 - V2.0 (Ulrich Radig mail@ulrichradig.de)
-//             - Anpassung an neuere WinAVR Version
-//             - Anpassung an meine Platinen Version
-//
-// Copyright (C) 2001,2002,2003 Mathias Dzionsko (madz@gmx.de)
-//===========================================================================
+/**
+ * minidmx - RS232-to-DMX-converter
+ *
+ * Copyright 2001,2002,2003 Mathias Dzionsko <madz@gmx.de>
+ *
+ * Compiler:
+ *   WinAVR <http://sourceforge.net/projects/winavr/>
+ *   AVR Studio <http://www.atmel.com/avrstudio>
+ * MCU: ATTiny2313 (originally AT90S2313)
+ *
+ * Changelog:
+ *  2006-12-17 Ulrich Radig <http://ulrichradig.de/>
+ *    * Ported to ATTiny2313
+ *    * Adapted to current WinAVR
+ *    * Adapted to my PCB, see
+ *      <http://www.ulrichradig.de/home/index.php/avr/rs232-dmx>
+ *  2003-09-09 Mathias Dzionsko <http://www.dzionsko.de/>
+ *    * Fixed generation of DMX-signal
+ *  2003-03-21 Mathias Dzionsko <http://www.dzionsko.de/>
+ *    * Reimplemented the in C instead of assembler (v2.0)
+ *  2003-02-04 Mathias Dzionsko <http://www.dzionsko.de/>
+ *    * LED1 indicates successfully sent DMX data
+ *    * LED2 indicates an error with the RS232-communication
+ *    * LED3 indicates the end of the startup procedure (running and
+ *    * data can be sent)
+ *  2001-02-18 Mathias Dzionsko <http://www.dzionsko.de/>
+ *    * First public release (v1.0)
+ */
+
 #include <inttypes.h>
 #include <avr\io.h>
 #include <avr\interrupt.h>
@@ -30,18 +35,18 @@
 #define F_CPU           9216000
 #include <util/delay.h>
 //---------------------------------------------------------------------------
-// Makros
+// Macros
 
 #define BITS2BYTE(b7,b6,b5,b4,b3,b2,b1,b0) \
   (b7<<7|b6<<6|b5<<5|b4<<4|b3<<3|b2<<2|b1<<1|b0)
 
-// Verzögert den Programmablauf um t µs.
+// Delay by t µs.
 #define Delay16(l)      _delay_loop_2(l)
 #define DELAY16VAL(t)   ((XTAL*(long)(t))/4000)
 //---------------------------------------------------------------------------
-// Konstanten
+// Constants
 
-// Taktfrequenz der MCU in kHz (für Delay16)
+// Clock of the MCU in kHz (for Delay16)
 #define XTAL            9216
 
 #if defined (__AVR_ATtiny2313__)
@@ -56,21 +61,20 @@
 #define DMX_PORT        PORTB
 #define DMX_PIN         PB0
 
-// LED1 zeigt ein erfolgreiches Senden der DMX Daten an
-// LED2 zeigt Fehler beim Datenempfang vom PC an
-// LED3 zeigt das Ende das Startvorgangs an (Daten können nun
-// gesendet werden)
+// LED1 indicates a successfully sent DMX packet
+// LED2 indicates an error receiveing data via RS232
+// LED3 indicates end of startup procedure
 #define LED_PORT        PORTB
 #define LED1_PIN        PB2
 #define LED2_PIN        PB3
 #define LED3_PIN        PB4
 
-// Leuchtdauer der LEDs: 100ms (in 1/100s)
+// LED indication time: 100ms (in 1/100s)
 #define LED1_ONTIME     10
 #define LED2_ONTIME     10
 
 // Port B Pins
-// PB0 - DMX-Ausgang
+// PB0 - DMX output
 // PB1 - Switch
 // PB2 - LED1
 // PB3 - LED2
@@ -81,7 +85,7 @@
 // Port D Pins
 // PD0 - RXD
 // PD1 - TXD
-// PD6 - Triggersignal für Oszilloskop (zu Testzwecken)
+// PD6 - Testing trigger for an oscillosscope
 #define PORTD_INIT      BITS2BYTE(0,0,1,1,0,0,1,0)
 #define DDRD_INIT       BITS2BYTE(0,0,1,1,0,0,1,0)
 
@@ -96,7 +100,7 @@
 #define TCCR0_INIT      (_BV(CS02)+_BV(CS00))   // CK/1024 => 9000 Hz
 #define TIMER0_RELOAD   (256-90)                // => 100 Hz
 
-// Kommunikationsprotokoll
+// RS232-protocol
 #define P_BLKSTART      0x5A
 #define P_BLKEND        0xA5
 
@@ -107,18 +111,18 @@
 #define P_ERROR         0xC0
 #define P_ACK           0xC1
 
-// Konstanten für die Variable Error
+// Constants for the Error var
 #define E_BUFFEROVERFLOW        1
 #define E_UARTTIMEOUT           2
 #define E_PROTOCOLERROR         4
 //---------------------------------------------------------------------------
-// Globale Variablen
+// Global vars
 
 volatile uint8_t        TimerLED1, TimerLED2;   // LED Timer
 volatile uint8_t        UARTTimer;
 volatile uint8_t        Error;
 
-// Empgangspuffer für die serielle Schnittstelle
+// RS232 receive buffer
 uint8_t                 RxBuffer[RX_BUFSIZE];
 uint8_t                 RxBufferOut;
 volatile uint8_t        RxBufferIn;
@@ -127,19 +131,19 @@ volatile uint8_t        RxBufferIn;
 
 SIGNAL(SIG_OVERFLOW0)
 {
-  // Timer nachladen
+  // Reload timer
   TCNT0=TIMER0_RELOAD;
 
   // UARTTimer
   if (UARTTimer) UARTTimer--;
   
-  // LED1 ein/ausschalten
+  // Trigger LED1
   if (TimerLED1) {
     LED_PORT &= ~(1<<LED1_PIN);
     TimerLED1--;
   } else LED_PORT |= (1<<LED1_PIN);
 
-  // LED2 ein/ausschalten
+  // Trigger LED2
   if (TimerLED2) {
     LED_PORT &= ~(1<<LED2_PIN);
     TimerLED2--;
@@ -153,17 +157,17 @@ ISR (UART_TX_vect)
   uint8_t nextpos;
   unsigned char tmp ;
 
-  // Nächste Bufferposition ausrechnen
+  // Calculate next buffer position
   nextpos=RxBufferIn+1;
   if (nextpos==RX_BUFSIZE) nextpos=0;
 
-  // Ist der Buffer voll?
+  // Buffer full?
   if (nextpos==RxBufferOut) {
-    // Buffer ist übergelaufen
+    // Indicate error.
     Error|=E_BUFFEROVERFLOW;
-    tmp = UDR; // UART Data Register leeren
+    tmp = UDR; // Clear UART Data Register
   } else {
-    // Empfangenes Byte in den Buffer schreiben
+    // Store received data in buffer.
     RxBuffer[RxBufferIn]=UDR;
     RxBufferIn=nextpos;
   }
@@ -175,17 +179,17 @@ uint8_t UARTRecvByte(void)
 {
   uint8_t data;
 
-  // Warten bis ein Byte im Buffer oder die Zeit abgelaufen ist
+  // Wait until a byte arrives in the buffer or a timeout occurs
   UARTTimer=RX_TIMEOUT;
   while (RxBufferIn==RxBufferOut && UARTTimer) ;
 
-  // Kein Byte angekommen?
+  // No data received?
   if (RxBufferIn==RxBufferOut) {
     Error|=E_UARTTIMEOUT;
     return 0;
   }
-  
-  // Byte auslesen
+
+  // Read byte.
   data=RxBuffer[RxBufferOut++];
   if (RxBufferOut==RX_BUFSIZE) RxBufferOut=0;
 
@@ -196,31 +200,32 @@ uint8_t UARTRecvByte(void)
 
 void UARTSendByte(uint8_t data)
 {
-  // Warten bis das letzte Byte gesendet wurde
+  // Wait until the previous byte is completely transmitted.
   loop_until_bit_is_set(USR, UDRE);
 
-  // Byte senden
+  // Send new data.
   UDR=data;
 }
 //---------------------------------------------------------------------------
 // DMXSendByte
 
-// Sendet ein Datenbyte über die DMX-Leitung.
-// Die benötigten Takte für ein DMX-Bit sind 37. Die Taktfrequenz beträgt
-// 9,216 MHz. Das ergibt 4,0147 µs pro Bit. Die Abweichung beträgt dann 0,37
-// Prozent, welche innerhalb der zulässigen Toleranz liegt.
-// Interrupts werden für 44,3 µs nicht zugelassen.
+// Sends a DNX data byte.
+// The spec wants 4 µs per DMX bit.  With a clock frequency of
+//   9.216 MHz
+// we have to use up 37 cycles so we get 4.0147 µs per Bit which is ok.
+// Interrupts are blocked while this code runs.
+// We transmit 11 Bit, so we block the interrupts for about 44.3 µs.
 
 
 void DMXSendByte(uint8_t value)
 {
   asm volatile (
-"                cli                    \n" // keine Ints zulassen
+"                cli                    \n" // Block interrupts
 "                clc                    \n" // 1      - Start Bit
-"                rcall DMXSendBit       \n" // +3+33=37 Takte
+"                rcall DMXSendBit       \n" // +3+6+27=37 cycles
 "                                       \n"
 "                ror %2                 \n" // 1      - Bit 0
-"                rcall DMXSendBit       \n" // +3+33=37 Takte
+"                rcall DMXSendBit       \n" // +3+6+27=37 cycles
 "                ror %2                 \n" //        - Bit 1
 "                rcall DMXSendBit       \n"
 "                ror %2                 \n" //        - Bit 2
@@ -240,21 +245,21 @@ void DMXSendByte(uint8_t value)
 "                rcall DMXSendBit       \n"
 "                sec                    \n"  //        - Stop Bit 2
 "                rcall DMXSendBit       \n"
-"                sei                    \n"  // Ints wieder zulassen
+"                sei                    \n"  // Unblock interrupts
 "                rjmp DMXSendByteExit   \n"
 "                                       \n"
 "DMXSendBit:     brcs DMXSendBit1       \n"
 "                nop                    \n" // 1+1
 "                cbi %0, %1             \n" // +2
-"                rjmp DMXSendBit2       \n" // +2=6 Takte
+"                rjmp DMXSendBit2       \n" // +2=6 cycles
 "DMXSendBit1:    sbi %0, %1             \n" // 2+2
-"                rjmp DMXSendBit2       \n" // +2=6 Takte
+"                rjmp DMXSendBit2       \n" // +2=6 cycles
 "                                       \n"
 "DMXSendBit2:    ldi r31,7              \n" // 1
 "DMXSendBit3:    dec r31                \n" // +1
 "                brne DMXSendBit3       \n" // +1/+2
 "                rjmp DMXSendBit4       \n" // +2
-"DMXSendBit4:    ret                    \n" // +4=9+6*3=27 Takte
+"DMXSendBit4:    ret                    \n" // +4=9+6*3=27 cycles
 "DMXSendByteExit:                       \n"
 //                          %0               %1            %2
     : : "I" (_SFR_IO_ADDR(DMX_PORT)), "I" (DMX_PIN), "r" (value) : "r31"
@@ -264,18 +269,17 @@ void DMXSendByte(uint8_t value)
 //---------------------------------------------------------------------------
 // DMXSendReset
 
-// Sendet ein Reset-Signal mit anschließendem Start-Byte über die
-// DMX-Leitung.
+// Sends a DMX RESET followed by a start byte.
 
 void DMXSendReset(void)
 {
-  DMX_PORT &=~(1<<DMX_PIN);       // RESET-Signal senden
-  Delay16(DELAY16VAL(100));     // 100µs (min. 88µs) warten
+  DMX_PORT &=~(1<<DMX_PIN);       // Send RESET
+  Delay16(DELAY16VAL(100));     // Wait 100µs (min. 88µs)
 
-  DMX_PORT |= (1<<DMX_PIN);       // MARK-Signal senden
-  Delay16(DELAY16VAL(10));      // 10µs (min. 8µs) warten
+  DMX_PORT |= (1<<DMX_PIN);       // Send MARK
+  Delay16(DELAY16VAL(10));      // Wait 10µs (min. 8µs)
 
-  DMXSendByte(0);               // Startbyte (0) senden
+  DMXSendByte(0);               // Send START byte (0)
 }
 //---------------------------------------------------------------------------
 // DMXSendChannels
@@ -285,7 +289,7 @@ void DMXSendChannels(uint16_t cnt)
   uint8_t data;
 
   DMXSendReset();
-  
+
   while (cnt--) {
     data=UARTRecvByte();
     if (Error) return;
@@ -293,53 +297,53 @@ void DMXSendChannels(uint16_t cnt)
   }
 }
 //---------------------------------------------------------------------------
-// Hauptfunktion
+// Main
 
 int main(void)
 {
   uint8_t data;
 
-  // Port B initialisieren
+  // Initialize Port B
   PORTB=PORTB_INIT;
   DDRB=DDRB_INIT;
 
-  // Port D initialisieren
+  // Initialize Port D
   PORTD=PORTD_INIT;
   DDRD=DDRD_INIT;
-  
-  // UART initialisieren
+
+  // Initiualize UART
   UBRR=UBRR_INIT;
   UCR=UCR_INIT;
 
-  // LED-Timer initialisieren
+  // Initialize LED-Timer
   TCNT0=TIMER0_RELOAD;
   TCCR0=TCCR0_INIT;
   TIMSK=TIMSK_INIT;
 
-  // Interrupts zulassen
+  // Enable interrupts
   sei();
 
-  // LED3 einschalten
+  // Switch on LED3
   LED_PORT |=(1 << LED3_PIN);
 
-  // Hauptschleife
+  // Main loop
   while (1) {
     Error=0;
     data=UARTRecvByte();
     if (!Error) {
-      // Blockanfang?
+      // Command block received?
       if (data==P_BLKSTART) {
         data=UARTRecvByte();
         if (!Error) {
-          // Befehl vom PC ausführen
+          // Execute command.
           switch (data) {
-            case P_DMXOUT96: DMXSendChannels(96); break;
+            case P_DMXOUT96:  DMXSendChannels(96);  break;
             case P_DMXOUT256: DMXSendChannels(256); break;
             case P_DMXOUT512: DMXSendChannels(512); break;
             default: Error|=E_PROTOCOLERROR;
           }
           if (!Error) {
-            // Stimmt das Blockende?
+            // Block ok?
             data=UARTRecvByte();
             if (data!=P_BLKEND) Error|=E_PROTOCOLERROR;
           }
@@ -347,16 +351,16 @@ int main(void)
       } else Error|=E_PROTOCOLERROR;
     }
     if (Error) {
-      // Fehler anzeigen
+      // Indicate error.
       TimerLED2=LED2_ONTIME;
-      // Fehler an den PC senden
+      // Send error reply to master.
       UARTSendByte(P_BLKSTART);
       UARTSendByte(P_ERROR);
       UARTSendByte(P_BLKEND);
     } else {
-      // Befehl erfolgreich bearbeitet
+      // Indicate success.
       TimerLED1=LED1_ONTIME;
-      // Bestätigung an den PC senden
+      // Send succes reply to master.
       UARTSendByte(P_BLKSTART);
       UARTSendByte(P_ACK);
       UARTSendByte(P_BLKEND);
